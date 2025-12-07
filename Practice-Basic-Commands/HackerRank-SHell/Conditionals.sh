feb36827f5dd7f29b236bc6a82ad2dcfa583e521
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-07 23:11:53 +0600

    Add conditionals script for yes/no input
    This script reads a character from input and displays 'YES' for 'Y' or 'y', and 'NO' for 'N' or 'n'.

---
+++ Practice-Basic-Commands/HackerRank-SHell/Conditionals.sh
@@ -0,0 +1,7 @@
+#Read in one character from STDIN. If the character is 'Y' or 'y' display "YES". If the character is 'N' or 'n' display "NO". No other character will be provided as input.
+read c
+if [[ "$c" == "Y" || "$c" == "y" ]]; then
+    echo "YES"
+else
+    echo "NO"
+fi
