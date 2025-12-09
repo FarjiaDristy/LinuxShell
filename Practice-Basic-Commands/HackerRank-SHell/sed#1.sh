ecdf80083cd36b1c7c1e762e81f35b631597833e
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-09 23:18:10 +0600

    Create sed#1.sh
    replace the first occurrence of 'the' to 'this'

---
+++ Practice-Basic-Commands/HackerRank-SHell/sed#1.sh
@@ -0,0 +1,3 @@
+#For each line in a given input file, transform the first occurrence of the word 'the' with 'this'. The search and transformation should be strictly case sensitive.
+#sol:
+sed 's/the /this /1'
