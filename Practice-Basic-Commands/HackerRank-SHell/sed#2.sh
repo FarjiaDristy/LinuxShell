9cc73bdc58b0d5d4734f57976f3db54a65f94435
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-09 23:41:11 +0600

    Update sed#2.sh

--- Practice-Basic-Commands/HackerRank-SHell/sed#2.sh
+++ Practice-Basic-Commands/HackerRank-SHell/sed#2.sh
@@ -1,3 +1,5 @@
 #For each line in a given input file, transform all the occurrences of the word 'thy' with 'your'. The search should be case insensitive, i.e. 'thy', 'Thy', 'tHy' etc. should be transformed to 'your'.
 #sol:
 sed 's/thy /your /Ig'
+#or
+sed 's/\bthy\b/your/Ig'
