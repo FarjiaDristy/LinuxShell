21a9b48bbeaa5674cd2450d4bf386c136226fc4a
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-10 23:13:34 +0600

    Create array#1.sh

---
+++ Practice-Basic-Commands/HackerRank-SHell/array#1.sh
@@ -0,0 +1,4 @@
+#Given a list of countries, each on a new line, your task is to read them into an array and then display the element indexed at 3. Note that indexing starts from 0.
+#sol:
+mapfile -t countries
+echo "${countries[3]}"
