26af5b467592bc4ae12d27ab80feec6767be3a4c
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-10 22:57:12 +0600

    Create awk#1.sh

---
+++ Practice-Basic-Commands/HackerRank-SHell/awk#1.sh
@@ -0,0 +1,8 @@
+#You are given a file with four space separated columns containing the scores of students in three subjects. The first column contains a single character (a-z), the student identifier. The next three columns have three numbers each. The numbers are between 1 and 100, both inclusive. These numbers denote the scores of the students in English, Mathematics, and Science, respectively.
+
+#sol:
+
+awk '{
+if($2 == "" || $3 == "" || $4 == "")
+print "Not all scores are available for", $1, "";
+}'
