834af2e1d6a7bbe87904fc03062bc6ce4b0f14a2
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-10 23:39:16 +0600

    Create array#3.sh

---
+++ Practice-Basic-Commands/HackerRank-SHell/array#3.sh
@@ -0,0 +1,7 @@
+#You are given a list of countries, each on a new line. Your task is to read them into an array and then transform them in the following way: The first capital letter (if present) in each element of the array should be replaced with a dot ('.'). Then, display the entire array with a space between each country's names.
+#sol:
+mapfile -t countries
+for i in "${!countries[@]}"; do
+    countries[$i]=$(echo "${countries[$i]}" | sed 's/[A-Z]/./')
+done
+echo "${countries[@]}"
