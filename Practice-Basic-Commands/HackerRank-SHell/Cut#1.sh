ded8204705033ed746fc25b00d0195ad676794cf
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-14 00:41:28 +0600

    Update Cut#1.sh

--- Practice-Basic-Commands/HackerRank-SHell/Cut#1.sh
+++ Practice-Basic-Commands/HackerRank-SHell/Cut#1.sh
@@ -1,5 +1,6 @@
 #Given N lines of input, print the 3rd character from each line as a new line of output. It is guaranteed that each of the n lines of input will have a 3rd character.
 #sol1:
+#!/bin/bash
 cut -c3
 #sol2:
 while read line; do
