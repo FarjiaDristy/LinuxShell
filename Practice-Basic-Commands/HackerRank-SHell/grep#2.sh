dba323683a0d7d5a6db5c0e4978dfaadc98085ba
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-18 21:16:39 +0600

    Create grep#2.sh
    Output only those lines that contain the word 'the'. The search should NOT be case sensitive. The relative ordering of the lines in the output should be the same as it was in the input.

---
+++ Practice-Basic-Commands/HackerRank-SHell/grep#2.sh
@@ -0,0 +1,8 @@
+#You are given a text file that will be piped into your command through STDIN. Use grep to display all those lines that contain the word the in them. 
+#The search should NOT be sensitive to case. Display only those lines of the input file that contain the word 'the'.
+
+#sol:
+#!/bin/bash
+grep -i -w 'the'
+#or
+grep -iw 'the'
