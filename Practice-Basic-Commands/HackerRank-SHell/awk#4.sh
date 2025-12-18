10b23673ba7997ef73dfda58191c05c304c7d620
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-18 21:08:39 +0600

    Create awk#4.sh
    Concatenate every 2 lines of input with a semi-colon.

---
+++ Practice-Basic-Commands/HackerRank-SHell/awk#4.sh
@@ -0,0 +1,6 @@
+#You are provided a file with four space-separated columns containing the scores of students in three subjects. The first column, contains a single character (A-Z) - the identifier of the student. The next three columns have three numbers (each between 0 and 100, both inclusive) which are the scores of the students in English, Mathematics and Science respectively.
+#sol:
+#!bin/bash
+awk '{ 
+    printf "%s%s", $0, (NR % 2 == 0 ? "\n" : ";")
+}'
